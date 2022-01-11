687498dc93b89d1d016fcf9f677893ddf85ecf32
Adam Turnbull <adam@adamt.com>
2022-01-11 20:51:08 +0000

    Bump to 0.1.1

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject carme "0.1.0-SNAPSHOT"
+(defproject carme "0.1.1"
   :description "Carme, a Gemini server written in Clojure."
   :url "gemini://spikydinosaur.com"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure       "1.10.3"]]
-;;                 [com.novemberain/pantomime "2.11.0"]]
   :repl-options {:init-ns carme.core}
   :profiles {:uberjar {:aot :all}}
   :main carme.core)
